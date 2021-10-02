30762b17a9cce794db5c24fe5120884d2e2d0ede
jungeun-p <mrk7753@naver.com>
2021-10-02 23:48:55 +0900

    [Modified] photos/[id]/index 생성 및 이미지 클릭시 id에 따른 이동
    - photos/[id]/index.js 생성

--- next-practice/next.config.js
+++ next-practice/next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["via.placeholder.com"],
+  },
+};
+// 외부 url을 사용할 경우 추가
--- next-practice/pages/index.js
+++ next-practice/pages/index.js
@@ -16,21 +16,10 @@ export default function Home({ posts }) {
 // 페이지에 들어올 때마다 서버에 요청 후 데이터를 받아옴
 // 서버에서 만든 html 파일을 제공
 
-export const getServerSideProps = async () => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_start=0&_end=10`
-  );
-  const posts = await res.json();
-  return {
-    props: {
-      posts,
-    },
-  };
-};
-
-// export const getStaticProps = async () => {
+// export const getServerSideProps = async () => {
 //   const res = await fetch(
-//     `https://jsonplaceholder.typicode.com/posts?_start=0&_end=10`
+//     // `https://jsonplaceholder.typicode.com/posts?_start=0&_end=10`
+//     `http://localhost:8080/api/posts`
 //   );
 //   const posts = await res.json();
 //   return {
@@ -39,3 +28,18 @@ export const getServerSideProps = async () => {
 //     },
 //   };
 // };
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_start=0&_end=10`
+    // http://localhost:8080/api/posts`
+  );
+  const posts = await res.json();
+  return {
+    props: {
+      posts,
+    },
+    // 20초 지난 시점부터 접속이 일어나면 파일을 새롭게 생성
+    revalidate: 20,
+  };
+};
